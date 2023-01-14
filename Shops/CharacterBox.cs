4e7196cdfd511ca12f3109a52bf88cd38c4b70f3
Марк Немирович <2591192@gmail.com>
2023-01-14 11:38:57 +0300

    Shop upgraded

--- Shops/CharacterBox.cs
+++ Shops/CharacterBox.cs
@@ -2,18 +2,21 @@
 {
     internal class CharacterBox
     {
-        public readonly string Name;
-        public readonly int Cost;
-        public readonly BoxSize Size;
-        public CharacterBox(string name, int cost, BoxSize size)
+        private string _name;
+        private int _cost;
+        private Currency _currency;
+        private BoxSize _size;
+        public CharacterBox(string name, int cost, Currency currency, BoxSize size)
         {
-            Name = name;
-            Cost = cost;
-            Size = size;
+            _name = name;
+            _cost = cost;
+            _currency = currency;
+            _size = size;
         }
+        public string Name() => $"{_name} for {_currency.ToString().ToLower()}";
         public string Info()
         {
-            return $"\nName = {Name}\nCost = {Cost} money\nCard will be provided = {(int)Size}\n";
+            return $"\nName = {_name}\nCost = {_cost} money\nCard will be provided = {(int)_size}\n";
         }
     }
 }
